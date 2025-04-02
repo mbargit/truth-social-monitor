1e26dd490d859436d00c0d87606f2279376a91a7
mbargit <trickerzyt@gmail.com>
2025-04-02 15:38:24 +0100

    done

--- py_tls/py/tlsexpert.py
+++ py_tls/py/tlsexpert.py
@@ -16,6 +16,14 @@ import atexit
 # Set the name of our shared library
 lib_name = 'tlsexpert'
 
+# Telegram configuration
+TELEGRAM_BOT_TOKEN = "7841049730:AAHUvJpCgaEClEvWVqHw-MlKwxAwKze5n-k"
+TELEGRAM_CHAT_IDS = [
+    "-1002393083645",  # First chat ID
+    # "-1002589438564",  # Second chat ID (replace with your actual chat ID)
+    # "-1002314755584"   # Third chat ID (replace with your actual chat ID)
+]
+
 VALID_CLIENT_HELLOS = [
   'Firefox_55',
   'Firefox_56',
@@ -244,15 +252,6 @@ def restart_script():
 def run_monitor():
     """Main monitoring function"""
     url = "https://truthsocial.com/api/v1/accounts/114253527119250506/statuses?exclude_replies=true&only_replies=false&with_muted=true"
-    
-    # Telegram configuration
-    TELEGRAM_BOT_TOKEN = "7841049730:AAHUvJpCgaEClEvWVqHw-MlKwxAwKze5n-k"
-    # List of three different chat IDs
-    TELEGRAM_CHAT_IDS = [
-        "-1002393083645",  # First chat ID
-        # "-1002589438564",  # Second chat ID (replace with your actual chat ID)
-        # "-1002314755584"   # Third chat ID (replace with your actual chat ID)
-    ]
     
     headers = {
         'accept': 'application/json, text/plain, */*',
